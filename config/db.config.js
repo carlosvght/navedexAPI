d6ebf310705814d110bad11896679a7c762d9c85
Carlos <carlosvaghetti01@gmail.com>
2021-03-06 21:27:03 -0300

    configured app, created files and folders, server running

---
+++ config/db.config.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/nave-challenge';
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  // autoReconnect: true,
+  useCreateIndex: true,
+  poolSize: 15,
+  keepAlive: true,
+  keepAliveInitialDelay: 270000
+}
+
+mongoose.connect(uri, options)
+.then(() => {
+  console.log('Mongodb Connected!')
+
+  mongoose.connection.on('error', (err) => {
+    console.log('mongoose connection on error ::;' , err)
+  })
+  mongoose.connection.on('reconnected', () => {
+    console.log('Reconnected to MongoDB!')
+  })
+})
+.catch((err) => {
+  console.log('mongoose connection rejected promise ::; ', err)
+  mongoose.disconnect()
+})
+
+
+module.exports = mongoose;
---
+++ modules/Project/route/project.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router()
+const projectController = require('../controller/project')
+
+module.exports = router;
