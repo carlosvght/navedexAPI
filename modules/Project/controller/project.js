b7c62ba686a3a65167381b7f73fa966638543e8e
Carlos <carlosvaghetti01@gmail.com>
2021-03-08 00:03:59 -0300

    project update working

--- modules/Project/controller/project.js
+++ modules/Project/controller/project.js
@@ -25,9 +25,26 @@ const find = async(req, res, next) => {
   }
 }
 
+const update = async(req, res, next) => {
+  try {
+    const query = {_id:req.params.id}
+    const projectReqBody = req.body 
+    const name = req.body.name
+    const projectObjectAttributesToUpdate = {
+      name
+    }
+    const updateProject = await projectModel.findByIdAndUpdate(query, projectObjectAttributesToUpdate)
+    res.status(200).json(updateProject)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error)
+  }
+}
+
 const objectModuleToExports = {
   create,
-  find
+  find,
+  update
 }
 
 module.exports = objectModuleToExports
--- modules/Project/route/project.js
+++ modules/Project/route/project.js
@@ -4,5 +4,6 @@ const projectController = require('../controller/project')
 
 router.post('/project/create', projectController.create)
 router.get('/project/:id', projectController.find)
+router.put('/project/update/:id', projectController.update)
 
 module.exports = router;
